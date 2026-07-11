69841757d1b1c01f86a3be677cb83a649928c0d8
KrivonZa <kevintruong6587@gmail.com>
2026-07-11 16:26:28 +0700

    Initial commit

---
+++ nook-cafe-be/src/main/java/com/nookcafe/nookcafe/NookCafeBeApplication.java
@@ -0,0 +1,13 @@
+package com.nookcafe.nookcafe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NookCafeBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NookCafeBeApplication.class, args);
+    }
+
+}
---
+++ nook-cafe-be/src/test/java/com/nookcafe/nookcafe/NookCafeBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nookcafe.nookcafe;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NookCafeBeApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
